390b29fb485d046b288291831b31c84ece365085
Vladimir Golovnin <gaalez@ya.ru>
2017-08-26 13:23:57 +0400

    hw15. Admin page sharing is done.

--- hw15/src/main/java/ru/otus/java_2017_04/golovnin/hw15/Frontend/AdminServlet.java
+++ hw15/src/main/java/ru/otus/java_2017_04/golovnin/hw15/Frontend/AdminServlet.java
@@ -22,26 +22,22 @@ public class AdminServlet extends HttpServlet{
     private static final String pageFile = "/admin.html";
     private static final String PATH_STORAGE_ATTRIBUTE_NAME = "redirectedPath";
     private static final String LOGIN_FORM_PATH_ATTRIBUTE_NAME = "loginFormPath";
-    private static final String APP_CONTEXT_ATTRIBUTE_NAME = "appContext";
     private static final String AUTH_SERVICE_ATTRIBUTE_NAME = "authService";
 
     private String loginPath;
     private AuthenticationService authService;
-    private String page;
+    private String page = "";
 
     @Override
     public void init() throws ServletException {
         super.init();
         ServletContext servletContext = this.getServletContext();
         loginPath = (String) servletContext.getAttribute(LOGIN_FORM_PATH_ATTRIBUTE_NAME);
-        ApplicationContext appContext = (ApplicationContext) servletContext.getAttribute(APP_CONTEXT_ATTRIBUTE_NAME);
         authService = (AuthenticationService) servletContext.getAttribute(AUTH_SERVICE_ATTRIBUTE_NAME);
-        page = "empty";
 
         try (InputStream is = servletContext.getResourceAsStream(pageFile)){
             if(is != null){
                 BufferedReader pageReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                pageReader.skip(1);
                 StringBuilder pageBuilder = new StringBuilder();
                 String nextLine;
                 while ((nextLine = pageReader.readLine()) != null){
@@ -60,9 +56,10 @@ public class AdminServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         if(authService.isAuthorized(session)){
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("text/html");
             resp.getWriter().println(page);
             resp.setStatus(HttpServletResponse.SC_OK);
-            resp.setContentType("text/html; charset=UTF-8");
         }
         else{
             session.setAttribute(PATH_STORAGE_ATTRIBUTE_NAME, req.getContextPath());
